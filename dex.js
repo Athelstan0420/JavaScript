08ce74f7115d337609c380823e56cb88efbb4d03
Adriel Medeiros <130292427+Athelstan0420@users.noreply.github.com>
2024-08-13 02:30:02 -0300

    Add files via upload

---
+++ dex.js
@@ -0,0 +1,93 @@
+
+/*
+for(e = 10; e > 0;  e = e - 1) {
+    document.writeln(e , '\n')
+
+}
+*/ 
+/*
+e = 10
+while(e > 0) {
+    document.writeln(e)
+    e--
+}
+*/
+/*
+var soma = 0
+for (e= 1 ; e < 51; e = e + 1 ){
+    if (e % 2 == 0) {
+        soma = soma + e
+    }
+}
+document.write(soma)
+*/
+
+/*
+var i = 0
+var soma = 0
+while (i < 50) {
+    i++
+    if (i % 2 == 0) {
+        soma = soma + i
+    }
+}
+document.write(soma)
+
+*/
+
+
+/*
+var num
+num = prompt("Digite o valor: ")
+
+for (i=0; i<11; i = i + 1) {
+    mult = num * i
+    document.write(mult + "\n")
+}
+*/
+
+/*
+var num
+var i = 0
+num = prompt("Digite o valor: ")
+while (i < 10) {
+    i++
+    mult = num * i
+    document.write(mult + "\n")
+    
+}
+*/
+
+/*
+var fat = 1
+for(e = 10; e > 0;  e = e - 1) {
+    fat = fat * e
+    document.write(fat, "\n")
+}
+*/
+
+i = 11
+fat = 1
+while(i > 0) {
+    i -= 1
+    fat = fat * i
+    document.write(fat, "\n")
+}
+
+
+
+
+/*
+for (i = 5; i < 50; i = i + 5){
+    document.write(i + '\n')
+}
+*/
+
+/*
+var i = 5
+while(i < 50) {
+    i += 5
+    document.write(i + '\n')
+}
+*/
+
